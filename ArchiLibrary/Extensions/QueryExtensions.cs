08993af11c0b600800a251372ac24320a0a6f5ff
Ryme LEHNA <sutrils-nozu@outlook.fr>
2022-11-10 12:30:20 +0100

    Ajout des filtres

--- ArchiLibrary/Extensions/QueryExtensions.cs
+++ ArchiLibrary/Extensions/QueryExtensions.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.ObjectiveC;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -55,5 +56,39 @@ namespace ArchiLibrary.Extensions
                 return (IOrderedQueryable<TModel>)query.Skip(start).Take((end-start) + 1);
                 //return query.OrderBy(x => x.Name);
         }
+
+        public static IQueryable<TModel> Filter<TModel>(this IQueryable<TModel> query, Params p, Dictionary<string, string> arrayProperties)
+        {
+            //arrayProperties = arrayProperties ?? throw new ArgumentNullException(nameof(arrayProperties));
+            BinaryExpression binaryExpression = null;
+            var parameter = Expression.Parameter(typeof(TModel), "x");
+            foreach (var item in arrayProperties)
+            {
+                if (!string.IsNullOrWhiteSpace(item.Key) && !string.IsNullOrWhiteSpace(item.Value))
+                {
+                    var key = item.Key;
+                    var value = item.Value;
+                    var c = Expression.Constant(value);
+                    var property = Expression.Property(parameter, key /*"Name"*/);
+                    var o = Expression.Convert(property, typeof(object));
+                    var lambda = Expression.Equal(o, c);
+                    if (binaryExpression == null)
+                    {
+                        binaryExpression = lambda;
+                    }
+                    else
+                        binaryExpression = Expression.And(binaryExpression, lambda);
+                    // var equal = Expression.Equal(olamba,value);
+                    //return await _context.Set<TModel>().Where(x => x.Name.Contains(param.Name)).ToListAsync();
+                }
+            }
+            if (binaryExpression != null)
+            {
+                return query.Where(Expression.Lambda<Func<TModel, bool>>(binaryExpression, parameter));
+            }
+            else
+                return (IQueryable<TModel>)query;
+        }
+       
     }
 }
