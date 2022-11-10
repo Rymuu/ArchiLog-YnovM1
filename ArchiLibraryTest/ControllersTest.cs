1c634485286ddb475ea0a2775f5b2a135e2ffa59
HaLan0101 <dnhalan0101@gmail.com>
2022-11-10 12:31:07 +0100

    ajout Recherche finie

---
+++ ArchiLibraryTest/ControllersTest.cs
@@ -0,0 +1,13 @@
+using ArchiLibrary.Controllers;
+
+namespace ArchiLibraryTest
+{
+    public class ControllersTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ ArchiLogTest/BrandControllerTest.cs
@@ -0,0 +1,11 @@
+using Archilog.Controllers;
+using ArchiLog.Data;
+using ArchiLog.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ArchiLogTest
+{
+    public class BrandControllerTest
+    {
+    }
+}
